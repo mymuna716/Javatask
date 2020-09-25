36f38f30bb23fde63649237058c6f61efe00d9b4
Maimoona <70699060+mymuna716@users.noreply.github.com>
2020-09-25 12:59:55 +0530

    Create Reverse.java

---
+++ Reverse.java
@@ -0,0 +1,10 @@
+
+public class Reverse {
+	public static void main(String[] args) {
+		
+		StringBuffer sb=new StringBuffer("The world is empty");
+		System.out.println("Before Reverse : "+sb);
+		sb.reverse();
+		System.out.println("After Reverse : "+sb);
+	}
+}
